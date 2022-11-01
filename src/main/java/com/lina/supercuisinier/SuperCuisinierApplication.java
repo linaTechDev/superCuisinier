7fe75c821e0b9cd33e4081496f77afb773455587
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-11-01 10:05:27 -0400

    initialisation projet 2 (react, spring)

---
+++ src/main/java/com/lina/supercuisinier/SuperCuisinierApplication.java
@@ -0,0 +1,13 @@
+package com.lina.supercuisinier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCuisinierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperCuisinierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lina/supercuisinier/SuperCuisinierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lina.supercuisinier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperCuisinierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
