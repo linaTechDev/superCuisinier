cce40eac401c155e23fe2c0f681e565d42a1edfa
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-11-06 20:09:15 -0500

    Changement de signature class a record (ConnectedUser, CreatedUser)

--- src/main/java/com/lina/supercuisinier/models/ConnectedUser.java
+++ src/main/java/com/lina/supercuisinier/models/ConnectedUser.java
@@ -1,18 +1,3 @@
 package com.lina.supercuisinier.models;
 
-import lombok.Data;
-
-@Data
-public class ConnectedUser {
-    private String token;
-    private String prenom;
-    private String nomFamille;
-    private String nomUtilisateur;
-
-    public ConnectedUser(String token, String prenom, String nomFamille, String nomUtilisateur) {
-        this.token = token;
-        this.prenom = prenom;
-        this.nomFamille = nomFamille;
-        this.nomUtilisateur = nomUtilisateur;
-    }
-}
+public record ConnectedUser(String token, String prenom, String nomFamille, String nomUtilisateur) {}
--- src/main/java/com/lina/supercuisinier/models/CreatedUser.java
+++ src/main/java/com/lina/supercuisinier/models/CreatedUser.java
@@ -1,16 +1,4 @@
 package com.lina.supercuisinier.models;
 
-import lombok.Data;
 
-@Data
-public class CreatedUser {
-    private String prenom;
-    private String nomFamille;
-    private String nomUtilisateur;
-
-    public CreatedUser(String prenom, String nomFamille, String nomUtilisateur) {
-        this.prenom = prenom;
-        this.nomFamille = nomFamille;
-        this.nomUtilisateur = nomUtilisateur;
-    }
-}
+public record CreatedUser(String prenom, String nomFamille, String nomUtilisateur) {}
